886e81b64dfc8ac155c5c6c7211b0477d960e15f
Allan Munatsi <Allanmnts30@gmail.com>
2025-03-01 12:29:55 +0200

    making the navbar responsive

--- src/components/FoodDisplay/FoodDispaly.jsx
+++ src/components/FoodDisplay/FoodDispaly.jsx
@@ -15,7 +15,7 @@ const {food_list} = useContext(StoreContext)
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/> 
               }
              
-        })}+
+        })}
       </div>
     </div>
   )
