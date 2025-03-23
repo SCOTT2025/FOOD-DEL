82884e0ddd332c70715add7ef10631e6497830f4
Allan Munatsi <Allanmnts30@gmail.com>
2025-03-23 14:03:26 +0200

    Set up backend server with Express and CORS; update package.json for module type

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-n
+import express from 'express';
+import cors from 'cors';
+
+
+//app config
+const app = express();
+const port = 4000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API working fine');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
+
+// mongodb+srv://swiftbite:302323@cluster0.txnfp.mongodb.net/?
